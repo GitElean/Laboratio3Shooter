616cbb63df4e62444824fc5221713b9f6d524dfb
GitElean <eleanjulian@gmail.com>
2021-04-29 20:24:25 -0600

    Initial commit

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Color original;
+    [SerializeField]  private float force = 0.0f;
+   
+    Rigidbody rb;
+    void Start()
+    {
+        original = GetComponent<MeshRenderer>().material.color;
+
+        rb = GetComponent< Rigidbody > ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<MeshRenderer>().material.color = original;
+    }
+
+    private void OnMouseDown()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo;
+
+        Physics.Raycast(camRay, out hitinfo);
+
+        if (rb)
+            rb.AddForceAtPosition(-hitinfo.normal * force, hitinfo.point,ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int score=0;
+    public Text scoreText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitinfo;
+        Ray camRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        Physics.Raycast(camRay);
+
+        if (Physics.Raycast(camRay, out hitinfo) && Input.GetMouseButtonDown(0)) 
+        {
+            if (hitinfo.collider.CompareTag("Target"))
+            {
+                Destroy(hitinfo.collider.gameObject);
+                score++;
+            }
+            
+        }
+
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
